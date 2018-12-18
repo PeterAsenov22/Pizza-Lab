9e9f6730e65739479d69e0ecefa000bf30e2a896
PeterAsenov22 <peter12345@abv.bg>
2018-12-18 16:51:59 +0200

    RegisterInputModel modified

--- PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Account/InputModels/RegisterInputModel.cs
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Account/InputModels/RegisterInputModel.cs
@@ -5,23 +5,18 @@
     public class RegisterInputModel
     {
         private const int PasswordMinLength = 8;
-        private const int UsernameMinLength = 3;
-        private const int NameMinLength = 2;
+        private const int UsernameMinLength = 4;
 
         [Required]
         [EmailAddress(ErrorMessage = "Please enter valid e-mail address.")]
         public string Email { get; set; }
 
         [Required]
-        [MinLength(UsernameMinLength, ErrorMessage = "Username should be at least 3 symbols long.")]
+        [MinLength(UsernameMinLength, ErrorMessage = "Username should be at least 4 symbols long.")]
         public string Username { get; set; }
 
-        [Required]
-        [MinLength(NameMinLength, ErrorMessage = "First name should be at least 2 symbols long.")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MinLength(NameMinLength, ErrorMessage = "Last name should be at least 2 symbols long.")]
         public string LastName { get; set; }
 
         [Required]
