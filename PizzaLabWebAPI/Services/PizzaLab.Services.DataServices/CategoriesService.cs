7d93ee3505736f254feec15fdd258ec856fc82fd
PeterAsenov22 <peter12345@abv.bg>
2018-12-22 15:11:36 +0200

    CategoriesService added

---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/CategoriesService.cs
@@ -0,0 +1,45 @@
+﻿namespace PizzaLab.Services.DataServices
+{
+    using Contracts;
+    using Data.Common;
+    using Data.Models;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly IRepository<Category> _categoriesRepository;
+
+        public CategoriesService(IRepository<Category> categoriesRepository)
+        {
+            this._categoriesRepository = categoriesRepository;
+        }
+
+        public bool Any()
+        {
+            return this._categoriesRepository.All().Any();
+        }
+
+        public async Task CreateAsync(string categoryName)
+        {
+            var category = new Category()
+            {
+                Name = categoryName
+            };
+
+            await this._categoriesRepository.AddAsync(category);
+            await this._categoriesRepository.SaveChangesAsync();
+        }
+
+        public async Task CreateRangeAsync(string[] categoriesName)
+        {
+            var categories = categoriesName.Select(categoryName => new Category()
+            {
+                Name = categoryName
+            });
+
+            await this._categoriesRepository.AddRangeAsync(categories);
+            await this._categoriesRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/Contracts/ICategoriesService.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaLab.Services.DataServices.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface ICategoriesService
+    {
+        bool Any();
+
+        Task CreateAsync(string categoryName);
+
+        Task CreateRangeAsync(string[] categoriesName);
+    }
+}
