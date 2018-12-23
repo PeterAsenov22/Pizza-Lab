bf1d4d3674a0098cce461faad2cfbbc2f955a626
PeterAsenov22 <peter12345@abv.bg>
2018-12-23 21:42:57 +0200

    IngredientsService added

---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/Contracts/IIngredientsService.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaLab.Services.DataServices.Contracts
+{
+    using Data.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IIngredientsService
+    {
+        bool Any();
+
+        IEnumerable<Ingredient> All();
+
+        Task CreateAsync(string ingredientName);
+
+        Task CreateRangeAsync(string[] ingredientsName);
+    }
+}
---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/IngredientsService.cs
@@ -0,0 +1,51 @@
+﻿namespace PizzaLab.Services.DataServices
+{
+    using Contracts;
+    using Data.Common;
+    using Data.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class IngredientsService : IIngredientsService
+    {
+        private readonly IRepository<Ingredient> _ingredientsRepository;
+
+        public IngredientsService(IRepository<Ingredient> ingredientsRepository)
+        {
+            this._ingredientsRepository = ingredientsRepository;
+        }
+
+        public bool Any()
+        {
+            return this._ingredientsRepository.All().Any();
+        }
+
+        public IEnumerable<Ingredient> All()
+        {
+            return this._ingredientsRepository.All();
+        }
+
+        public async Task CreateAsync(string ingredientName)
+        {
+            var ingredient = new Ingredient
+            {
+                Name = ingredientName
+            };
+
+            await this._ingredientsRepository.AddAsync(ingredient);
+            await this._ingredientsRepository.SaveChangesAsync();
+        }
+
+        public async Task CreateRangeAsync(string[] ingredientsName)
+        {
+            var ingredients = ingredientsName.Select(ingredientName => new Ingredient
+            {
+                Name = ingredientName
+            });
+
+            await this._ingredientsRepository.AddRangeAsync(ingredients);
+            await this._ingredientsRepository.SaveChangesAsync();
+        }
+    }
+}
