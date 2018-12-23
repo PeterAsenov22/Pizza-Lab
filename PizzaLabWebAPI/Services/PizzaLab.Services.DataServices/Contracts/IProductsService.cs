2787441573afb8371308fa42f5c81f1348751950
PeterAsenov22 <peter12345@abv.bg>
2018-12-23 21:43:18 +0200

    ProductsService added

---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/Contracts/IProductsService.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaLab.Services.DataServices.Contracts
+{
+    using Data.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;    
+
+    public interface IProductsService
+    {
+        bool Any();
+
+        Task CreateAsync(Product product);
+
+        Task CreateRangeAsync(IEnumerable<Product> products);
+    }
+}
---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/ProductsService.cs
@@ -0,0 +1,36 @@
+﻿namespace PizzaLab.Services.DataServices
+{
+    using Contracts;
+    using Data.Common;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore.Internal;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class ProductsService : IProductsService
+    {
+        private readonly IRepository<Product> _productsRepository;
+
+        public ProductsService(IRepository<Product> productsRepository)
+        {
+            this._productsRepository = productsRepository;
+        }
+
+        public bool Any()
+        {
+            return this._productsRepository.All().Any();
+        }
+
+        public async Task CreateAsync(Product product)
+        {
+            await this._productsRepository.AddAsync(product);
+            await this._productsRepository.SaveChangesAsync();
+        }
+
+        public async Task CreateRangeAsync(IEnumerable<Product> products)
+        {
+            await this._productsRepository.AddRangeAsync(products);
+            await this._productsRepository.SaveChangesAsync();
+        }
+    }
+}
