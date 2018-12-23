448af12fc28fb3ac37fc6c960d67e2f48e3c0b25
PeterAsenov22 <peter12345@abv.bg>
2018-12-23 22:00:49 +0200

    Tests for IngredientsService added

---
+++ PizzaLabWebAPI/Tests/PizzaLab.Services.DataServices.Tests/IngredientsServiceTests.cs
@@ -0,0 +1,74 @@
+﻿namespace PizzaLab.Services.DataServices.Tests
+{
+    using Data;
+    using Data.Common;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class IngredientsServiceTests
+    {
+        private readonly IRepository<Ingredient> _ingredientsRepository;
+        private readonly IngredientsService _ingredientsService;
+
+        public IngredientsServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<PizzaLabDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var dbContext = new PizzaLabDbContext(options);
+            this._ingredientsRepository = new EfRepository<Ingredient>(dbContext);
+            this._ingredientsService = new IngredientsService(_ingredientsRepository);
+        }
+
+        [Fact]
+        public async Task AnyShouldReturnTrue()
+        {
+            await _ingredientsService.CreateAsync("ham");
+
+            Assert.True(_ingredientsService.Any());
+        }
+
+        [Fact]
+        public void AnyShouldReturnFalse()
+        {
+            Assert.False(_ingredientsService.Any());
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldCreateCategorySuccessfully()
+        {
+            await _ingredientsService.CreateAsync("ham");
+
+            Assert.Equal(1, _ingredientsRepository.All().Count());
+            Assert.Equal("ham", _ingredientsRepository.All().First().Name);
+        }
+
+        [Fact]
+        public async Task CreateRangeAsyncShouldCreateCategorySuccessfully()
+        {
+            await _ingredientsService.CreateRangeAsync(new string[]
+            {
+                "ham", "mushrooms"
+            });
+
+            Assert.Equal(2, _ingredientsRepository.All().Count());
+            Assert.Equal("ham", _ingredientsRepository.All().First().Name);
+            Assert.Equal("mushrooms", _ingredientsRepository.All().Last().Name);
+        }
+
+        [Fact]
+        public async Task AllShouldReturnCorrectValues()
+        {
+            await _ingredientsService.CreateRangeAsync(new string[]
+            {
+                "ham", "mushrooms"
+            });
+
+            Assert.Equal(_ingredientsService.All(), _ingredientsRepository.All());
+        }
+    }
+}
