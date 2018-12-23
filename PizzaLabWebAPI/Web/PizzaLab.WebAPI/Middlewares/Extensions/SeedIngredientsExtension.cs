967250cef2ba07005497081896477b43e41bc8de
PeterAsenov22 <peter12345@abv.bg>
2018-12-23 21:43:59 +0200

    Ingredients seed middleware added

---
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Middlewares/Extensions/SeedIngredientsExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaLab.WebAPI.Middlewares.Extensions
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class SeedIngredientsExtension
+    {
+        public static IApplicationBuilder UseSeedIngredientsMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SeedIngredientsMiddleware>();
+        }
+    }
+}
---
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Middlewares/SeedIngredientsMiddleware.cs
@@ -0,0 +1,62 @@
+﻿namespace PizzaLab.WebAPI.Middlewares
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Services.DataServices.Contracts;
+    using System;
+    using System.Threading.Tasks; 
+    
+    public class SeedIngredientsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SeedIngredientsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IServiceProvider provider)
+        {
+            var ingredientsService = provider.GetService<IIngredientsService>();
+            if (!ingredientsService.Any())
+            {
+                await ingredientsService.CreateRangeAsync(new string[]
+                {
+                    "olive oil",
+                    "oregano",
+                    "pepperoni salami",
+                    "yellow cheese",
+                    "tomato sauce",
+                    "ham",
+                    "mushrooms",
+                    "smoked cheese",
+                    "traditional bulgarian flat sausage called lukanka",
+                    "chicken roll",
+                    "corn",
+                    "red peppers",
+                    "hot peppers",
+                    "chicken",
+                    "avocado",
+                    "olives",
+                    "pineapple",
+                    "white bulgarian cheese",
+                    "blue cheese",
+                    "philadelphia",
+                    "tuna fish",
+                    "white pepper",
+                    "cherry tomatoes",
+                    "basil chips",
+                    "chorizo",
+                    "proschuitto",
+                    "eggs",
+                    "bacon",
+                    "red onion",
+                    "mozzarella",
+                    "parsley"
+                });
+            }
+
+            await this._next(context);
+        }
+    }
+}
