58219dcc775a07476c0e1da5d5d30bdc1b5b863c
PeterAsenov22 <peter12345@abv.bg>
2018-12-25 21:47:07 +0200

    ReviewsService added

---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/Contracts/IReviewsService.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaLab.Services.DataServices.Contracts
+{
+    using Data.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IReviewsService
+    {
+        Task<Review> CreateAsync(string text, string creatorId, string productId);
+
+        IEnumerable<Review> GetProductReviews(string productId);
+    }
+}
---
+++ PizzaLabWebAPI/Services/PizzaLab.Services.DataServices/ReviewsService.cs
@@ -0,0 +1,46 @@
+﻿namespace PizzaLab.Services.DataServices
+{
+    using Contracts;
+    using Data.Common;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ReviewsService : IReviewsService
+    {
+        private readonly IRepository<Review> _reviewsRepository;
+
+        public ReviewsService(IRepository<Review> reviewsRepository)
+        {
+            this._reviewsRepository = reviewsRepository;
+        }
+
+        public async Task<Review> CreateAsync(string text, string creatorId, string productId)
+        {
+            var review = new Review
+            {
+                Text = text,
+                CreatorId = creatorId,
+                ProductId = productId,
+                LastModified = DateTime.Now
+            };
+
+            await this._reviewsRepository.AddAsync(review);
+            await this._reviewsRepository.SaveChangesAsync();
+
+            return review;
+        }
+
+        public IEnumerable<Review> GetProductReviews(string productId)
+        {
+            return this._reviewsRepository
+                .All()
+                .Include(r => r.Creator)
+                .Where(r => r.ProductId == productId)
+                .ToList();
+        }
+    }
+}
