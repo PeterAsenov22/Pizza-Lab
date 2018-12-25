97e5d0b10f5e9a6a95ce50b57f14027a1cf922cc
PeterAsenov22 <peter12345@abv.bg>
2018-12-25 21:48:47 +0200

    ProductsController added

---
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿namespace PizzaLab.WebAPI.Controllers
+{
+    using AutoMapper;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Models.Products.ViewModels;
+    using Services.DataServices.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;  
+
+    [Route("api/[controller]/[action]")]
+    public class ProductsController : ApiController
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductsService _productsService;
+
+        public ProductsController(
+            IMapper mapper,
+            IProductsService productsService)
+        {
+            this._mapper = mapper;
+            this._productsService = productsService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<ProductViewModel>> All()
+        {
+            return this._productsService
+                .All()
+                .Select(p => this._mapper.Map<ProductViewModel>(p))
+                .ToList();
+        }
+    }
+}
---
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Products/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PizzaLab.WebAPI.Models.Products.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Weight { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Image { get; set; }
+
+        public string Category { get; set; }
+
+        public IEnumerable<string> Ingredients { get; set; }
+
+        public IEnumerable<string> Likes { get; set; }
+    }
+}
