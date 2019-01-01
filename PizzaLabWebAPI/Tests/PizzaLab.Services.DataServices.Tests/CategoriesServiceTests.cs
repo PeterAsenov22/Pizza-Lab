57d62e59e6693b90175f40f18515debd5054a352
PeterAsenov22 <peter12345@abv.bg>
2019-01-01 23:41:56 +0200

    CategoriesServiceTests updated

--- PizzaLabWebAPI/Tests/PizzaLab.Services.DataServices.Tests/CategoriesServiceTests.cs
+++ PizzaLabWebAPI/Tests/PizzaLab.Services.DataServices.Tests/CategoriesServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace PizzaLab.Services.DataServices.Tests
 {
+    using AutoMapper;
     using Data;
     using Data.Common;
     using Data.Models;
@@ -7,6 +8,7 @@
     using System;
     using System.Linq;
     using System.Threading.Tasks;
+    using WebAPI.Helpers;
     using Xunit;
 
     public class CategoriesServiceTests
@@ -20,22 +22,13 @@
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
             var dbContext = new PizzaLabDbContext(options);
-            this._categoriesRepository = new EfRepository<Category>(dbContext);
-            this._categoriesService = new CategoriesService(_categoriesRepository);
-        }
 
-        [Fact]
-        public async Task AnyShouldReturnTrue()
-        {
-            await _categoriesService.CreateAsync("Traditional");
+            var mapperProfile = new MappingConfiguration();
+            var conf = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
+            var mapper = new Mapper(conf);
 
-            Assert.True(_categoriesService.Any());
-        }
-
-        [Fact]
-        public void AnyShouldReturnFalse()
-        {           
-            Assert.False(_categoriesService.Any());
+            this._categoriesRepository = new EfRepository<Category>(dbContext);            
+            this._categoriesService = new CategoriesService(_categoriesRepository, mapper);
         }
 
         [Fact]
@@ -61,12 +54,54 @@
         }
 
         [Fact]
+        public void FindByNameShouldReturnNull()
+        {
+            var category = _categoriesService.FindByName("Traditional");
+            Assert.Null(category);
+        }
+
+        [Fact]
         public async Task FindByNameShouldReturnCorrectValues()
         {
             await _categoriesService.CreateAsync("Traditional");
 
             var category = _categoriesService.FindByName("Traditional");
             Assert.Equal("Traditional", category.Name);
+        }
+
+        [Fact]
+        public void AllShouldReturnEmptyCollection()
+        {
+            var allCategories = _categoriesService.All().ToList();
+            Assert.Empty(allCategories);
+        }
+
+        [Fact]
+        public async Task AllShouldReturnCorrectValues()
+        {
+            await _categoriesService.CreateRangeAsync(new string[]
+            {
+                "Traditional", "Vegan"
+            });
+
+            var allCategories = _categoriesService.All().ToList();
+            Assert.Equal(2, allCategories.Count);
+            Assert.Equal("Traditional", allCategories.First().Name);
+            Assert.Equal("Vegan", allCategories.Last().Name);
+        }
+
+        [Fact]
+        public async Task AnyShouldReturnTrue()
+        {
+            await _categoriesService.CreateAsync("Traditional");
+
+            Assert.True(_categoriesService.Any());
+        }
+
+        [Fact]
+        public void AnyShouldReturnFalse()
+        {
+            Assert.False(_categoriesService.Any());
         }
     }
 }
