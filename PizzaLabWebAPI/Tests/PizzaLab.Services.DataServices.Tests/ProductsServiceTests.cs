e03299a0f090ab15c40f47974728031b3d9955fe
PeterAsenov22 <peter12345@abv.bg>
2019-01-04 17:04:18 +0200

    ProductsServiceTests updated

--- PizzaLabWebAPI/Tests/PizzaLab.Services.DataServices.Tests/ProductsServiceTests.cs
+++ PizzaLabWebAPI/Tests/PizzaLab.Services.DataServices.Tests/ProductsServiceTests.cs
@@ -1,53 +1,43 @@
 ﻿namespace PizzaLab.Services.DataServices.Tests
 {
+    using AutoMapper;
     using Data;
     using Data.Common;
     using Data.Models;
     using Microsoft.EntityFrameworkCore;
+    using Models.Products;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using WebAPI.Helpers;
     using Xunit;
 
     public class ProductsServiceTests
     {
         private readonly IRepository<Product> _productsRepository;
         private readonly ProductsService _productsService;
+        private readonly PizzaLabDbContext _dbContext;
 
         public ProductsServiceTests()
         {
             var options = new DbContextOptionsBuilder<PizzaLabDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
-            var dbContext = new PizzaLabDbContext(options);
-            this._productsRepository = new EfRepository<Product>(dbContext);
-            this._productsService = new ProductsService(_productsRepository);
-        }
+            _dbContext = new PizzaLabDbContext(options);
 
-        [Fact]
-        public async Task AnyShouldReturnTrue()
-        {
-            var product = new Product
-            {
-                Name = "Diablo"
-            };
+            var mapperProfile = new MappingConfiguration();
+            var conf = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
+            var mapper = new Mapper(conf);
 
-            await _productsService.CreateAsync(product);
-
-            Assert.True(_productsService.Any());
-        }
-
-        [Fact]
-        public void AnyShouldReturnFalse()
-        {
-            Assert.False(_productsService.Any());
+            this._productsRepository = new EfRepository<Product>(_dbContext);
+            this._productsService = new ProductsService(_productsRepository, mapper);
         }
 
         [Fact]
         public async Task CreateAsyncShouldCreateProductSuccessfully()
         {
-            var diablo = new Product
+            var diablo = new ProductDto
             {
                 Name = "Diablo",
                 Description = "Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas. If you like spicy hot and chilli flavours you will enjoy this pizza.",
@@ -71,9 +61,9 @@
         [Fact]
         public async Task CreateRangeAsyncShouldCreateProductsSuccessfully()
         {
-            var products = new List<Product>();
+            var products = new List<ProductDto>();
 
-            var pollo = new Product
+            var pollo = new ProductDto
             {
                 Name = "Pollo",
                 Description = "Pollo might be your choice when you are in the mood for something healthy. Tender grilled chicken, creamy feta, roasted red peppers and corn are generously piled on top of our famous tomato sauce.",
@@ -83,7 +73,7 @@
             };
             products.Add(pollo);
 
-            var diablo = new Product
+            var diablo = new ProductDto
             {
                 Name = "Diablo",
                 Description = "Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas. If you like spicy hot and chilli flavours you will enjoy this pizza.",
@@ -113,6 +103,13 @@
         }
 
         [Fact]
+        public void AllShouldReturnEmptyCollection()
+        {
+            var allProducts = _productsService.All().ToList();
+            Assert.Empty(allProducts);
+        }
+
+        [Fact]
         public async Task AllShouldReturnCorrectValues()
         {
             var products = new List<Product>();
@@ -124,12 +121,25 @@
                 Price = 10.90m,
                 Weight = 550,
                 Image = "http://www.ilforno.bg/45-large_default/polo.jpg",
-                Ingredients = new List<ProductsIngredients>(),
-                Category = new Category(),
-                Likes = new List<UsersLikes>(),
-                Reviews = new List<Review>()
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "Traditional"
+                },
+                Ingredients = new List<ProductsIngredients>
+                {
+                    new ProductsIngredients
+                    {
+                        IngredientId = 1,
+                        Ingredient = new Ingredient
+                        {
+                            Name = "ham"
+                        }
+                    }
+                },
+                Reviews = new List<Review>(),
+                Likes = new List<UsersLikes>()
             };
-            products.Add(pollo);
 
             var diablo = new Product
             {
@@ -138,16 +148,75 @@
                 Price = 8.90m,
                 Weight = 500,
                 Image = "https://images.pizza33.ua/products/product/yQfkJqZweoLn9omo68oz5BnaGzaIE0UJ.jpg",
-                Ingredients = new List<ProductsIngredients>(),
-                Category = new Category(),
-                Likes = new List<UsersLikes>(),
-                Reviews = new List<Review>()
+                Category = new Category
+                {
+                    Id = 2,
+                    Name = "Premium"
+                },
+                Ingredients = new List<ProductsIngredients>
+                {
+                    new ProductsIngredients
+                    {
+                        IngredientId = 1,
+                        Ingredient = new Ingredient
+                        {
+                            Name = "ham"
+                        }
+                    }
+                },
+                Reviews = new List<Review>(),
+                Likes = new List<UsersLikes>()
             };
+
+            products.Add(pollo);
             products.Add(diablo);
 
-            await _productsService.CreateRangeAsync(products);
+            await this._productsRepository.AddRangeAsync(products);
+            await this._productsRepository.SaveChangesAsync();
+   
+            var allProducts = _productsService.All().ToList();
+         
+            Assert.Equal(2, allProducts.Count);
+         
+            var firstProduct = allProducts.First();
+            Assert.Equal("Pollo", firstProduct.Name);
+            Assert.Equal("Pollo might be your choice when you are in the mood for something healthy. Tender grilled chicken, creamy feta, roasted red peppers and corn are generously piled on top of our famous tomato sauce.", firstProduct.Description);
+            Assert.Equal(10.90m, firstProduct.Price);
+            Assert.Equal(550, firstProduct.Weight);
+            Assert.Equal("http://www.ilforno.bg/45-large_default/polo.jpg", firstProduct.Image);
+            Assert.Equal("Traditional", firstProduct.Category.Name);
+         
+            var secondProduct = allProducts.Last();
+            Assert.Equal("Diablo", secondProduct.Name);
+            Assert.Equal("Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas. If you like spicy hot and chilli flavours you will enjoy this pizza.", secondProduct.Description);
+            Assert.Equal(8.90m, secondProduct.Price);
+            Assert.Equal(500, secondProduct.Weight);
+            Assert.Equal("https://images.pizza33.ua/products/product/yQfkJqZweoLn9omo68oz5BnaGzaIE0UJ.jpg", secondProduct.Image);
+            Assert.Equal("Premium", secondProduct.Category.Name);
+        }
 
-            Assert.Equal(_productsRepository.All().Count(), _productsService.All().Count());
+        [Fact]
+        public async Task AnyShouldReturnTrue()
+        {
+            var product = new ProductDto
+            {
+                Name = "Pollo",
+                CategoryId = 9,
+                Description = "Pollo might be your choice when you are in the mood for something healthy. Tender grilled chicken, creamy feta, roasted red peppers and corn are generously piled on top of our famous tomato sauce.",
+                Price = 10.90m,
+                Weight = 550,
+                Image = "http://www.ilforno.bg/45-large_default/polo.jpg"
+            };
+
+            await _productsService.CreateAsync(product);
+
+            Assert.True(_productsService.Any());
+        }
+
+        [Fact]
+        public void AnyShouldReturnFalse()
+        {
+            Assert.False(_productsService.Any());
         }
 
         [Fact]
@@ -159,7 +228,7 @@
         [Fact]
         public async Task ExistsShouldReturnTrue()
         {
-            var diablo = new Product
+            var diablo = new ProductDto
             {
                 Name = "Diablo",
                 Description = "Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas. If you like spicy hot and chilli flavours you will enjoy this pizza.",
@@ -172,6 +241,80 @@
             var productId = _productsRepository.All().First().Id;
 
             Assert.True(_productsService.Exists(productId));
+        }
+
+        [Fact]
+        public async Task EditAsyncShouldEditProductSuccessfully()
+        {
+            var product = new Product
+            {
+                Id = "1234",
+                Name = "Pollo",
+                Description = "Pollo might be your choice when you are in the mood for something healthy. Tender grilled chicken, creamy feta, roasted red peppers and corn are generously piled on top of our famous tomato sauce.",
+                Price = 10.90m,
+                Weight = 550,
+                Image = "http://www.ilforno.bg/45-large_default/polo.jpg",
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "Traditional"
+                },
+                Ingredients = new List<ProductsIngredients>
+                {
+                    new ProductsIngredients
+                    {
+                        IngredientId = 1,
+                        Ingredient = new Ingredient
+                        {
+                            Name = "ham"
+                        }
+                    }
+                },
+                Reviews = new List<Review>(),
+                Likes = new List<UsersLikes>()
+            };
+            await this._productsRepository.AddAsync(product);
+            await this._productsRepository.SaveChangesAsync();
+            _dbContext.Entry(product).State = EntityState.Detached;
+
+            var productDto = new ProductDto
+            {
+                Id = "1234",
+                Name = "Diablo2",
+                Description = "Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas.",
+                Price = 9.90m,
+                Weight = 400,
+                Image = "http://images.pizza33.ua/products/product/yQfkJqZweoLn9omo68oz5BnaGzaIE0UJ.jpg"
+            };
+
+            await _productsService.EditAsync(productDto);
+
+            var editedProduct = _productsRepository.All().AsNoTracking().First();
+            Assert.Equal("Diablo2", editedProduct.Name);
+            Assert.Equal("Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas.", editedProduct.Description);
+            Assert.Equal(9.90m, editedProduct.Price);
+            Assert.Equal(400, editedProduct.Weight);
+            Assert.Equal("http://images.pizza33.ua/products/product/yQfkJqZweoLn9omo68oz5BnaGzaIE0UJ.jpg", editedProduct.Image);
+        }
+
+        [Fact]
+        public async Task DeleteAsyncShouldDeleteProductSuccessfully()
+        {
+            var productDto = new ProductDto
+            {
+                Id = "1234",
+                Name = "Diablo",
+                Description = "Pizza diavola means the devils pizza and is quite a spicy little devil and one of my favourite pizzas. If you like spicy hot and chilli flavours you will enjoy this pizza.",
+                Price = 8.90m,
+                Weight = 500,
+                Image = "https://images.pizza33.ua/products/product/yQfkJqZweoLn9omo68oz5BnaGzaIE0UJ.jpg"
+            };
+
+            await _productsService.CreateAsync(productDto);
+            Assert.Equal(1, _productsRepository.All().Count());
+
+            await _productsService.DeleteAsync(productDto.Id);
+            Assert.Equal(0, _productsRepository.All().Count());
         }
     }
 }
