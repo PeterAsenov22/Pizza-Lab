47384e666d6568526a91a10c86959271339d25c1
PeterAsenov22 <peter12345@abv.bg>
2019-01-05 16:12:38 +0200

    Validation added to OrderInputModel

--- PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Orders/InputModels/OrderInputModel.cs
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Orders/InputModels/OrderInputModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace PizzaLab.WebAPI.Models.Orders.InputModels
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public class OrderInputModel
     {
+        [Required]
         public IEnumerable<OrderProductInputModel> OrderProducts { get; set; }
     }
 }
--- PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Orders/InputModels/OrderProductInputModel.cs
+++ PizzaLabWebAPI/Web/PizzaLab.WebAPI/Models/Orders/InputModels/OrderProductInputModel.cs
@@ -1,13 +1,26 @@
 ﻿namespace PizzaLab.WebAPI.Models.Orders.InputModels
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class OrderProductInputModel
     {
+        private const double MinimumPrice = 0.1;
+        private const int MinimumQuantity = 1;
+
+        private const string PriceErrorMessage = "Price should be a positive number.";
+        private const string QuantityErrorMessage = "Quantity should be a positive number.";
+
+        [Required]
         public string Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Range(MinimumPrice, Double.MaxValue, ErrorMessage = PriceErrorMessage)]
         public decimal Price { get; set; }
 
+        [Range(MinimumQuantity, int.MaxValue, ErrorMessage = QuantityErrorMessage)]
         public int Quantity { get; set; }
     }
 }
